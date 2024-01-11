a3f0f00cd76ee1c21467eac448677f5cfb87398b
Saifur Rahman <saifur2akash@gmail.com>
2024-01-12 00:44:21 +0600

    .

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qndasgicwcjeyulqnwrg.supabase.co'
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuZGFzZ2ljd2NqZXl1bHFud3JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI1MTcyNzUsImV4cCI6MjAwODA5MzI3NX0.yen-JllO8Rkujjuh-cDgAbF8ABe7Hcfal5uLGLt-nkM'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
